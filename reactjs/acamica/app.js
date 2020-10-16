d0448061a72aa38ab4a630ba9c4848693557c85e
José Manuel Aliaga <jmfaliaga@gmail.com>
2020-10-16 18:11:43 -0300

    starting learning reactjs

---
+++ reactjs/acamica/app.js
@@ -0,0 +1,11 @@
+var app = React.createElement('h1', null, 'React, loco - Hola Mundo!');
+
+ReactDOM.render(app, document.getElementById('content'));
+
+
+/* 
+Salida en HTML:
+ <div id="content">  
+ 
+ </div>
+*/

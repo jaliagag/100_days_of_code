7c3b2e1a131b1179eda6b5064c3e75045e5c50df
jaliagag <jmfaliaga@gmail.com>
2020-10-27 12:50:35 -0300

    working on 10 hour course on react

---
+++ 15REACTprs/notes.md
@@ -0,0 +1,3 @@
+# 15 REACT Projects
+
+<https://www.youtube.com/watch?v=a_7Z7C_JCyo>
--- reactjs/freecodecamp/my-app/src/index.js
+++ reactjs/freecodecamp/my-app/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// capitalize the first letter of a function to indicate React that it's a component!
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Greeting(){
+  return(
+    <h4>Hello world!</h4>
+  )
+}
+
+// looking for what we are going to render, Greeting, and where to render it, the html
+
+ReactDom.render(<Greeting />, document.getElementById('root'));
+
+// 1:04:11

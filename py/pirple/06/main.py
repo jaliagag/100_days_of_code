a45ccb3f93e504ff69f77b10ff03692f3f7427ad
jaliagag <jmfaliaga@gmail.com>
2020-11-03 00:23:56 -0300

    working on nested problem

--- py/pirple/06/main.py
+++ py/pirple/06/main.py
@@ -7,23 +7,35 @@
 
 #Try to determine the maximum width and height that your terminal and screen can comfortably fit without wrapping. If someone passes a value greater than either maximum, your function should return False.
 
-#rows = 5
-
-
-def tiktak(rows, columns):
+def tiktak(rows, columns): # 0, 1
+    menosUno = rows-1
     for row in range(rows):
         if row % 2 == 0:
             for column in range(1,columns):
                 if column % 2 == 1:
-                    if column != 5:
-                        print(" ",end="")
+                    if column != menosUno:
+                        print(" ", end="") # 0|1, 0|3
                     else:
-                        print(" ")
+                        print(" INTRO ") # 0|4
                 else:
-                    print("|")
+                    print("|", end="") # 0|0, 0|2
         else:
-            print("-----")
+            print("-----") # 1
     return True
 
-tiktak(5, 5)
+tiktak(10, 5)
 
+# print('########')
+
+#for row in range(5):
+#    if row % 2 == 0:
+     #   for column in range(1,6):
+    #        if column % 2 == 1:
+   #             if column != 5:
+  #                  print(" ",end="")
+ #               else:
+#                    print(" ")
+#            else:
+#                print("|", end="")
+#    else:
+#        print("-----")

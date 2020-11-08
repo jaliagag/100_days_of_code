cdcb5e42f58047767a55cef40bb8e825b32fd57b
José Manuel Aliaga <jmfaliaga@gmail.com>
2020-11-08 10:21:25 -0300

    back to reactjs

--- reactjs/freecodecamp/my-app/src/index.js
+++ reactjs/freecodecamp/my-app/src/index.js
@@ -1,16 +1,50 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
 
 // capitalize the first letter of a function to indicate React that it's a component!
 
-function Greeting(){
-  return(
-    <h4>Hello world!</h4>
-  )
+function Greeting() {
+	// to render a component, it must be capitalized
+	return (
+	  <>
+	    <section>
+	      <Book />
+	    </section>
+	  </>
+	);
+} // Greeting would be our root component
+
+//const Person = () => {
+//	return <h2>Jogn Doe</h2>;
+//};
+//const Message = () => <p>This is a message</p>;
+
+const Book = () => {
+  return <article>
+    <Image />
+  </article>
 }
+
+const Image = () => (
+  <img src='https://images-na.ssl-images-amazon.com/images/I/31vRcR-gi3L._SX248_BO1,204,203,200_.jpg' alt='' />
+)
+
+const Author = () =>
 
 // looking for what we are going to render, Greeting, and where to render it, the html
 
-ReactDom.render(<Greeting />, document.getElementById('root'));
+ReactDom.render(<Greeting />, document.getElementById("root"));
 
-// 1:04:11
+
+// function Greeting() --> stateless functional component or dumb components.
+// Always needs to return JSX.
+// JSX rules: we always have to return a SINGLE element
+// div / section / article or fragment
+// FRAGMENT: use <> ... </> = React.Fragment
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formatting
+// #####################
+// NESTED COMPONENTS
+//

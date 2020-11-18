d0e77dc5e01c1830e3e25d48e65b5b652b0228ab
José Manuel Aliaga <jmfaliaga@gmail.com>
2020-11-18 13:03:52 -0300

    finished ruleof 3

--- ruleof3pr/ruleof3/src/App.js
+++ ruleof3pr/ruleof3/src/App.js
@@ -1,47 +1,42 @@
+// https://es.reactjs.org/docs/forms.html
+// https://es.reactjs.org/docs/uncontrolled-components.html
+// https://medium.com/better-programming/the-complete-guide-to-forms-in-react-d2ba93f32825
+// https://scotch.io/courses/10-react-challenges-beginner/adding-calculator <<-- THE POST
+import React, {useState } from 'react';
+import ReactDOM from 'react-dom'
 import './App.css';
 
-const ruleOf3 = (num1, num2) => alert("Free % memory",(num2*100)/num1)
-
 function App() {
-  constructor(props){
-    super(props);
-    this.state = {value: ''};
-    
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [total, setTotal] = useState((number2*100)/number1)
+//  const [total, setTotal] = useState(number1 + number2)
 
-  handleChange(event){
-    this.setState({value:event.target.value});
-  }
-  
-  handleSubmit(event){
-    alert(this.state.value);
-    event.preventDefault();
-  }
-
+  const ruleOf3 = () => setTotal((number2*100)/number1)
   return (
     <>
-      <form onSubmit={this.handleSubmit}>
-        <p>Total Memory: 
+      <div className="boxes">
+        <p>Total Server Memory (in MB): 
 	  <input
 	    type="number"
 	    id="total"
-	    placeholder="total MB - e.g. 4000"
-	    value={this.state.value}
+	    value={number1}
+	    onChange= {e => setNumber1(+e.target.value)}
 	  />
 	</p>
-	<p>Free Memory: 
+	<p>Free Memory (in MB): 
 	  <input
 	    type="number"
 	    id="free"
-	    placeholder="total MB - e.g. 238"
-	    value={this.state.value}
-	    onChange={this.handleChange}
+	    value={number2}
+	    onChange= {e => setNumber2(+e.target.value)}
 	  />
 	</p>
-	<button id="calc">Calc</button>
-      </form>
+      </div>
+      <div className="outputs">
+	<button onClick={ruleOf3} id="calc">Calc</button>
+	<h1 id="tatales">Free memory: {total.toFixed(2)}%</h1>
+      </div>
     </>
   );
 }

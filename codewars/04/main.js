9bc1c21769fb2549452f2bf74a0c3190e50bf9bf
jaliagag <jmfaliaga@gmail.com>
2020-12-14 12:41:14 -0300

    finished challenge 4!

--- codewars/04/main.js
+++ codewars/04/main.js
@@ -1,7 +1,18 @@
 let highAndLow = (n) => {
-  let a = String(n)
-  return typeof a
-
+  let a = n.split(" ")
+  console.log(a)
+  let low = a[0]
+  let high = a[0]
+  for (const item of a){
+    console.log(item)
+    if (parseInt(item) > parseInt(high)) {
+      high = item
+    }
+    if (parseInt(item) < parseInt(low) ) {
+      low = item
+    }
+  }
+  return high + " " + low
 }
 
-console.log(highAndLow("1 2 3 4 5"))
+console.log(highAndLow(("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"), "542 -214"))

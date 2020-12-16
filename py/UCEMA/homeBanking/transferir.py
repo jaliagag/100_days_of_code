e35633982ac3723ebebfb0ddeeeae3eace13f294
José Manuel Aliaga <jmfaliaga@gmail.com>
2020-12-16 11:15:58 -0300

    add minor patches

--- py/UCEMA/homeBanking/transferir.py
+++ py/UCEMA/homeBanking/transferir.py
@@ -1,7 +1,10 @@
+import os
 import time
 from vars import y
 
-def realizarTransferencia(n):
+def realizarTransferencia(n, a):
+    # n is the amount to be transfered
+    # a is the receipent account
     file = open(y, "r")
     value = file.read()
     file.close()
@@ -21,12 +24,21 @@ def transf():
     a = 0
     while a == 0:
         b = int(input("¿Cuánta dinero quiere transferir? "))
-        c = input("¿A quién quiere transferir? (ingrese el alias o el CBUde la cuenta" )
-        print("Procesando la operación...")
-        time.sleep(1)
-        realizarTransferencia(b)
-        c = input("¿Desea realizar otra transferencia? (s / n) ")
-        if c == "s":
+        c = input("¿A quién quiere transferir? (ingrese el nombre de la cuenta) " )
+        check = os.path.isfile(c)
+        if check == True:
+            finalCheck = input("Está seguro de que desea transferir", b, "a la cuenta", c,"? (s / n)")
+            if finalCheck == "s":
+                print("Procesando la operación...")
+                time.sleep(1)
+                realizarTransferencia(b, c)
+            elif finalCheck == "n":
+                print("Cancelando la operación...")
+            d = input("¿Desea realizar otra transferencia? (s / n) ")
+            if d == "s":
+                a = 0
+            elif d == "n":
+                a = 1
+        else:
+            print("Error: no pudimos encontrar la cuenta", c)
             a = 0
-        elif c == "n":
-            a = 1
